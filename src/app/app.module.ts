2000d42a556ae36dc590e681234e34945099667e
wpupru <web@wpup.ru>
2017-10-19 18:54:57 -0500

    Добавлены cars и car,обновлено до 1.4.9
    Обновлен до версии 1.4.9 и добавлены два компонента - cars и car.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MyComponent} from './my/my.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/my/my.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-my',
+  templateUrl: './my.component.html',
+  styleUrls: ['./my.component.css']
+})
+
+export class MyComponent {
+  title = 'My first component';
+}
+
